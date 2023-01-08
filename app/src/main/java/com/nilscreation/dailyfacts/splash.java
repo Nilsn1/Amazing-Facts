4b48a653521e30c6f9de4487bf945bd79b7d04c5
admin <nileshsonawanen1@gmail.com>
2023-01-08 11:29:11 +0530

    final Changes

--- app/src/main/java/com/nilscreation/dailyfacts/splash.java
+++ app/src/main/java/com/nilscreation/dailyfacts/splash.java
@@ -6,14 +6,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class splash extends AppCompatActivity {
+
+    ImageView applogo;
+    TextView appname, subtext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        applogo = findViewById(R.id.applogo);
+        appname = findViewById(R.id.appname);
+        subtext = findViewById(R.id.subtext);
+
+        applogo.startAnimation(AnimationUtils.loadAnimation(splash.this, R.anim.fade_in));
+        appname.startAnimation(AnimationUtils.loadAnimation(splash.this, R.anim.fade_in));
+        subtext.startAnimation(AnimationUtils.loadAnimation(splash.this, R.anim.fade_in));
+
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         new Handler().postDelayed(new Runnable() {
             @Override
